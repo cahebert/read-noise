f45c5756db8e9f023ea90380c66e4973c23ae214
Julian Rovee <jrovee@stanford.edu>
2019-08-01 15:15:22 -0700

    added more documentation to unit conversion script

--- noise_unit_conversion.py
+++ noise_unit_conversion.py
@@ -7,7 +7,10 @@ c                      = 1.e8
 wavelength, throughput = np.genfromtxt('rbandThroughput.csv', unpack=True); wavelength *= 1.e-9
 freq                   = c / wavelength
 dfreq                  = -1 * np.diff(freq, prepend=0)
+Jy_to_SI               = 1.e-26
+nMgy_to_Jy             = 3.631e-6
 
-factor = 3.631 * 1.e-6 * 1.e-26 * area * collectionTime * np.dot(throughput,dfreq/freq) / h
+nMgy_to_ADU = nMgy_to_Jy * Jy_to_SI * area * collectionTime * np.dot(throughput,dfreq/freq) / h
+ADU_to_nMgy = 1 / nMgy_to_ADU
 
-print(1/factor)
+print(1/ADU_to_nMgy)
